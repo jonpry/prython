37c577c9a7e038085af430c069ac342492837bcb
Jon Pry <jonpry@gmail.com>
2019-12-25 16:51:44 -0500

    Missing file

---
+++ c_gen.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+om = ["float","str",
+                 "add","sub","mul","floordiv","truediv","mod","pow","lshift","rshift","and","xor","or",
+                 "radd","rsub","rmul","rfloordiv","rtruediv","rmod","rpow","rlshift","rrshift","rand","rxor","ror",
+                 "iadd","isub","imul","itruediv","ifloordiv","imod","ipow","ilshift","irshift","iand","ixor","ior",
+                 "neg","pos","abs","invert","complex","int","long","oct","hex",
+                 "index","round","trunc","floor","ceil",
+                 "enter","exit","iter","next",
+                 "lt","le","eq","ne","ge","gt",
+                 "new", "init", "del", "repr", "bytes", "format", "hash", "bool",
+                 "getattr","getattribute","setattr","delattr","dir",
+                 "get","set","delete","set_name","slots",
+                 "init_subclass","prepare","class","instancecheck","subclasscheck","class_getitem","call",
+                 "len","length_hint","getitem","setitem","delitem","missing","reverse","contains",
+                 "await","aiter","anext","aenter","aexit",
+                 ];
+
+
+print("""%{
+typedef struct SlotResult SlotResult;
+%}
+struct SlotResult
+  {
+  const char *name;
+  int slot_num;
+  };
+%%""")
+
+for i in range(len(om)):
+  print("__" + om[i] + "__, " + str(i)) 
+exit(0)
+
+for i in range(len(om)):
+   om[i] = om[i] + "__\x00"
+
+magic_methods = om[:]
+magic_methods.sort()
+#print(magic_methods)
+
+def indent(s,level):
+   for i in range(level):
+      s = "    " + s
+   print(s)
+
+def print_switch(level,prefix,idx):
+   last = ""
+   indent("  switch(str[" + str(level) + "]){",level)
+   for i in range(idx,len(magic_methods)):
+      if len(magic_methods[i]) - 1 < level:
+         continue
+      if level > 0 and magic_methods[i][:level] != magic_methods[idx][:level]:
+         break
+      if magic_methods[i][:level+1] != last:
+         last = magic_methods[i][:level+1]
+         txt = last[level] if last[level]!="\x00" else "\\x00"
+         indent("    case '" + txt + "': //" + str(level), level=level) #+ " - " + magic_methods[i],level)
+         oldlevel = level
+         if level < len(magic_methods[i])-1:
+            print_switch(level+1,magic_methods[i][:level+1],i)
+         else:
+            indent("      slot=" + str(om.index(magic_methods[i])) + ";", level)
+         assert(oldlevel == level)
+         indent("      break;",level)
+
+   indent("    default: break;",level)
+   indent("  }",level)
+
+print("int get_slot(const char * str){")
+print("  int slot=-1;")
+print_switch(0,"",0)
+print("  return slot;")
+print("}")
